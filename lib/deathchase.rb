70c040c5b40b70fa775738028ed759afe706d900
Gavin Laking <gavinlaking@gmail.com>
2015-04-29 21:58:55 +0100

    Draw a simple scene.

--- lib/deathchase.rb
+++ lib/deathchase.rb
@@ -1,5 +1,72 @@
-require "deathchase/version"
+require 'gosu'
 
 module Deathchase
-  # Your code goes here...
+
+  class GameWindow < Gosu::Window
+    def initialize
+      super(800, 600, false)
+      self.caption = "Deathchase"
+
+      @img_blacktree = Gosu::Image.new(self, media_path('blacktree.png'), false)
+      @img_whitetree = Gosu::Image.new(self, media_path('whitetree.png'), false)
+    end
+
+    def update
+    end
+
+    def draw
+      night_sky
+      grass
+      @img_blacktree.draw(320, -170, 0)
+      @img_blacktree.draw(-320, -80, 0)
+    end
+
+    private
+
+    def grass
+      x1 = 1
+      y1 = 300
+      x2 = 800
+      y2 = 300
+      x3 = 1
+      y3 = 600
+      x4 = 800
+      y4 = 600
+      c1 = Gosu::Color.new(25, 50, 60, 50)
+      c2 = Gosu::Color.new(25, 50, 60, 50)
+      c3 = Gosu::Color.new(40, 0, 200, 0)
+      c4 = Gosu::Color.new(40, 0, 200, 0)
+      z  = 0
+
+      draw_quad(x1, y1, c1, x2, y2, c2, x3, y3, c3, x4, y4, c4, z, :default)
+      draw_line(1, 300, Gosu::Color.new(25, 54, 64, 54), 800, 300, Gosu::Color.new(25, 54, 64, 54))
+      draw_line(1, 314, Gosu::Color.new(25, 56, 64, 56), 800, 314, Gosu::Color.new(25, 56, 64, 56))
+    end
+
+    def night_sky
+      x1 = 1
+      y1 = 1
+      x2 = 800
+      y2 = 1
+      x3 = 1
+      y3 = 300
+      x4 = 800
+      y4 = 300
+      c1 = Gosu::Color.new(40, 0, 0, 200)
+      c2 = Gosu::Color.new(40, 0, 0, 200)
+      c3 = Gosu::Color.new(25, 40, 40, 40)
+      c4 = Gosu::Color.new(25, 40, 40, 40)
+      z  = 0
+
+      draw_quad(x1, y1, c1, x2, y2, c2, x3, y3, c3, x4, y4, c4, z, :default)
+    end
+
+    def media_path(file_name = '')
+      File.dirname(__FILE__) + '/../lib/deathchase/media/' + file_name
+    end
+  end
+
 end
+
+window = Deathchase::GameWindow.new
+window.show
